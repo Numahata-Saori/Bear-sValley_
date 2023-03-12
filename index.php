688fea09255f3f8065430e971f676dceb6a6b2d6
Numahata-Saori <numahata.saori@gmail.com>
2023-03-12 22:59:37 +0900

    wordpress 2st step

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="shortcut icon" href="img/favicon.ico">
+</head>
+
+<body>
+  <div>
+    <ul>
+      <li><a href="page-nail.php">Nail</a></li>
+    </ul>
+  </div>
+</body>
+</html>
